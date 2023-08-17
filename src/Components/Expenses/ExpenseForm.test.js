65a30fe41bd0ac2e008c3c860744209d1464cb76
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-08-17 23:07:06 +0530

    Add Tese Case

--- src/Components/Expenses/ExpenseForm.test.js
+++ src/Components/Expenses/ExpenseForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen,fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../Store/store';
 import { BrowserRouter } from 'react-router-dom';
@@ -43,4 +43,43 @@ test('renders "Category" label in App component', () => {
   const priceLabel = getByText('Category:');
   expect(priceLabel).toBeInTheDocument();
 });
+test('updates input fields on change', () => {
+  const { getByLabelText} = render(
+    <Provider store={store}>
+      <BrowserRouter>
+    <ExpenseForm/>
+      </BrowserRouter>
+    </Provider>
+  );
+  const priceInput = screen.getByLabelText('Price:');
+ 
+
+  fireEvent.change(priceInput, { target: { value: '100' } });
+  expect(priceInput.value).toBe('100');
+  });
+
+
+test('updates input fields on change', () => {
+  const { getByLabelText} = render(
+    <Provider store={store}>
+      <BrowserRouter>
+    <ExpenseForm/>
+      </BrowserRouter>
+    </Provider>
+  );
+   const descriptionInput = screen.getByLabelText('Description:');
+   const categoryInput = screen.getByLabelText('Category:');
+
+   fireEvent.change(descriptionInput, { target: { value: 'Test description' } });
+   fireEvent.change(categoryInput, { target: { value: 'Food' } });
+
+    expect(descriptionInput.value).toBe('Test description');
+    expect(categoryInput.value).toBe('Food');
+  })
+
+  
+  
+
+
+
 })
